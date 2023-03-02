7cf387813a07d01f6822904c5d3fa0b43192a879
Mạnh Hùng <hungnguyenmanh2k2@gmail.com>
2023-03-02 13:10:18 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainpageComponent,
+    ProductsComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
